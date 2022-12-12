2e488c6fd15b8521493999050dd4a919e898eefb
hoda ghaffari <edu.ghaffari@gmail.com>
2022-12-12 16:14:47 +0330

    hgh: update store with register action (fetch) and etc

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -5,20 +5,38 @@ const store = createStore({
     state: {
         user: {
             data: {
-                name: 'Tom Cook',
-                email: 'tom@example.com',
-                imageUrl:
-                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
             },
-            token: '123'
+            token: sessionStorage.getItem("TOKEN")
         }
     },
     getters: {},
-    actions: {},
+    actions: {
+        register({commit}, user){
+            return fetch(`http://localhost:8000/api/register`,{
+              headers : {
+                "Content-Type":"application/json",
+                Accept:"application/json"
+              },
+              method : "POST",
+              body: JSON.stringify(user)
+            })
+            .then(res => res.json())
+            .then((res) => {
+                commit('setUser', res);
+                return res;
+            });
+        },
+    },
     mutations: {
         logout : (state) => {
             state.user.data= {};
             state.user.token = null;
+        },
+
+        setUser : (state, userDate) => {
+            state.user.token = userDate.token; 
+            state.user.data = userDate.user;
+            sessionStorage.setItem('TOKEN', userDate.token); //save token returened from laravel
         }
     },
     modules: {} 
